0a7f014ba49f8420e5efaa3df200f76ee0db4939
carneirofc <claudiofcarneiro@hotmail.com>
2019-02-25 11:08:56 -0300

    install scripts.

--- install.sh
+++ install.sh
@@ -1,33 +1,35 @@
 # Utility to manage display configurations
-sudo apt-get install arandr
+sudo apt-get install -y arandr
 
 # Utility to manage wallpapers
-sudo apt-get install feh
+sudo apt-get install -y feh
 
 # Brightness control
-sudo apt-get install xbacklight
+sudo apt-get install -y xbacklight
 
 # Touchpad etc ...
-sudo apt-get install libinput-tools
+sudo apt-get install -y libinput-tools
 
 # Theme manangment utility
-sudo apt-get install lxappearance
+sudo apt-get install -y lxappearance
 
-# Arc theme
-sudo apt-get install arc-theme
+# Arc themes
+sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get install -y arc-theme arc-icons
 
 # Icon theme
 sudo add-apt-repository -u ppa:snwh/ppa
-sudo apt-get install moka-icon-theme faba-icon-theme faba-mono-icons
+sudo apt-get install -y moka-icon-theme faba-icon-theme faba-mono-icons
 
 # Rofi - Application, window and ssh
-sudo apt-get install rofi
+sudo apt-get install -y rofi
 
 # Composite manager?
-sudo apt-get install compton
+sudo apt-get install -y compton
 
 # i3locks
-sudo apt-get install i3blocks
+sudo apt-get install -y i3blocks
 
 # i3blocks volume
-sudo apt-get install pavucontrol
+sudo apt-get install -y pavucontrol
